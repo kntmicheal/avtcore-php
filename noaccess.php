d0ef8504f028119517765c4e23228835232ff54d
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2014-07-02 10:05:27 +0200

    Cleaned up code base. Removed classes and files which are currently not developed and which have no test cases. These files will come in again when the tests are done and when they are needed in a real project.

--- noaccess.php
+++
@@ -1,3 +0,0 @@
-<? include("header.inc.php") ?>
-<?= __("Die angeforderte Seite steht Ihnen nicht zur Verf&uuml;gung. Bitte wenden Sie sich an den Administrator.") ?>
-<? include("footer.inc.php") ?>
